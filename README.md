d896d406d7b31403f80ab396dd148574bc4a4138
Austin <austin7955@hotmail.com>
2024-07-18 11:11:08 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Track-Workouts
+# Track Workouts
+
+Simple React web application for recording workouts, tracking weight in a graphical manner, and recording your personal best weights.
---
+++ workout-app/src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <div>
+          <p>This Section will contain the Workout tracker.</p>
+        </div>
+      </section>
+      <section>
+        <div className="card">
+          <p>This Section will contain track body weight.</p>
+        </div>
+      </section>
+      <section>
+        <p className="read-the-docs">
+          This Section will contain the Personal Best records.
+        </p>
+      </section>
+    </>
+  )
+}
+
+export default App
