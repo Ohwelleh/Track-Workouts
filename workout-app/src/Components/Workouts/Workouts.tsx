957f45dee97b7528d9994b726731b10603701967
Austin <austin7955@hotmail.com>
2024-07-20 15:55:24 -0700

    Implemented the logical for updating Workout Body based on the Workout Date selected.

--- workout-app/src/Components/Workouts/Workouts.tsx
+++ workout-app/src/Components/Workouts/Workouts.tsx
@@ -1,3 +1,6 @@
+// React hooks
+import { useState } from 'react'
+
 // Component imports.
 import { ListItem } from './ListItem'
 import { ListItemBody } from './ListItemBody'
@@ -6,30 +9,45 @@ import { ListItemBody } from './ListItemBody'
 import './Workouts Styles/workoutStyles.css'
 
 // Mock Data import
-import { WorkoutsMock, TestMock } from '../../Mock Data/MockData'
+import { WorkoutsMock, TestMock, WorkoutMock } from '../../Mock Data/MockData'
 import { Fragment } from 'react/jsx-runtime'
 
 export function Workouts(){
+    const initalStateVal: WorkoutMock = {
+        workoutID: 0,
+        workout: {
+            date: "Place holder", 
+            exercise: [
+                {name: "Place holder", weight: -225, reps: -10, sets: -4},
+            ]
+        },
+    }
+    const [workoutInfo, setWorkoutInfo] = useState<WorkoutMock>(initalStateVal)
     
     return(
-        <div className='workoutsMain'>
-            <div key={'workoutsDateDiv'} className='workoutDate'>
-                {TestMock.map((workout) => {
-                    return (
-                        <Fragment key={workout.workoutID}>
-                            <ListItem key={workout.workoutID} workID={workout.workoutID} date={workout.workout.date}/>
-                        </Fragment>
-                    )
-                })}
-            </div>
-            <div key={'workoutsBodyDiv'} className='workoutBody'>
-                {WorkoutsMock.map((workout) => {
+        <div className='gridContainer'>
+            <div className='workoutGrid'>
+                <div key={'workoutsDateDiv'} className='workoutDate'>
+                    {WorkoutsMock.map((workout) => {
                         return (
                             <Fragment key={workout.workoutID}>
-                                <ListItemBody key={workout.workoutID} workoutID={workout.workoutID} workout={workout.workout}/>
+                                <button className="dateBTN" onClick={e => setWorkoutInfo(workout)}>
+                                    <ListItem key={workout.workoutID} workID={workout.workoutID} date={workout.workout.date}/>
+                                </button>
                             </Fragment>
                         )
-                })}
+                    })}
+                </div>
+                <div key={'workoutsBodyDiv'} className='workoutBody'>
+                    <ListItemBody key={workoutInfo.workoutID} workoutID={workoutInfo.workoutID} workout={workoutInfo.workout} />
+                    {/* {WorkoutsMock.map((workout) => {
+                            return (
+                                <Fragment key={workout.workoutID}>
+                                    <ListItemBody key={workout.workoutID} workoutID={workout.workoutID} workout={workout.workout}/>
+                                </Fragment>
+                            )
+                    })} */}
+                </div>
             </div>
         </div>
     )
