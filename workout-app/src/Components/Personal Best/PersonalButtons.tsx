c8a2e27d07adef6dad3f32c861c46309478a88c2
Austin <austin7955@hotmail.com>
2024-07-29 11:34:52 -0700

    Initialized basic components for editing, adding, and deleting Personal best records.

--- workout-app/src/Components/Personal Best/PersonalButtons.tsx
+++ workout-app/src/Components/Personal Best/PersonalButtons.tsx
@@ -1,10 +1,13 @@
+// Form import
+import { PersonalAdd } from './Personal Form/PersonalAdd'
+
 // Styling import
 import './Personal Best Styles/personalStyles.css'
 
 export function PersonalButtons(){
     return(
         <div className='addDiv'>
-            <button className='personalAdd'>Add</button>
+            <button className='personalAdd' onClick={PersonalAdd}>Add</button>
         </div>
     )
 }
--- workout-app/src/Components/Personal Best/PersonalRecords.tsx
+++ workout-app/src/Components/Personal Best/PersonalRecords.tsx
@@ -1,30 +1,39 @@
+// Hook import
+import { useState } from 'react'
+
 // Component import
 import { PersonalButtons } from './PersonalButtons'
 
+// Forms import
+import { PersonalDelete } from './Personal Form/PersonalDelete'
+import { PersonalEdit } from './Personal Form/PersonalEdit'
 
 // Styling import
 import './Personal Best Styles/personalStyles.css'
 
 // Mock data for test
-import { PersonalMock } from '../../Mock Data/MockData'
+import { PersonalMock, PersonalRecord } from '../../Mock Data/MockData'
 
 export function PersonalRecords(){
+    const [editShow, setEditShow] = useState<boolean>(false)
+
     return(
         <div className='personalBestMainDiv'>
             <div className='personalBestTitle'>
                 <PersonalButtons />
             </div>
             <div className='personalBestRecords'>
-                {PersonalMock.map((exercise) => {
+                {PersonalMock.map((exercise, id) => {
                     return(
                         <div className="personalBestEntries">
                             <div className='personalDate'><p>{exercise.date}</p></div>
                             <div className='personalExercise'><p>{exercise.exercise}</p></div>
                             <div className='personalWeight'><p>{exercise.weight}</p></div>
                             <div className='personalBTNS'>
-                                <button className='editBTN'>Edit</button>
-                                <button>Delete</button>
+                                <button key={id} className='editBTN' onClick={() => setEditShow(!editShow)}>Edit</button>
+                                <button onClick={PersonalDelete} >Delete</button>
                             </div>
+                            {editShow && <PersonalEdit personalBest={exercise}/>}
                         </div>
                     )
                 })}
