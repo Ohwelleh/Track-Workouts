95222711fe9cb913533034c9ec9f5202ae6a73ed
Austin <austin7955@hotmail.com>
2024-08-02 11:49:49 -0700

    Implemented the functionality for Edit, Delete, and Add. Working on the testData array.

--- workout-app/src/Components/Personal Best/PersonalRecords.tsx
+++ workout-app/src/Components/Personal Best/PersonalRecords.tsx
@@ -12,19 +12,43 @@ import './Personal Best Styles/personalStyles.css'
 
 // Mock data for test
 import { PersonalMock, PersonalRecord } from '../../Mock Data/MockData'
+import { MouseEventHandler, useEffect, useState } from 'react'
 
 
 export function PersonalRecords(){
+    const TestingData = PersonalMock
+    const [testData, setTestData] = useState<PersonalRecord[]>(TestingData)
+
+    // Update Listener
+    useEffect(() => {console.log(testData, '- Has Changed!'), [testData]})
+
+    // Add row
+    function handleAdd(){
+        var newRow: PersonalRecord = {
+            exercise: '',
+            weight: '',
+            date: ''
+        }
+
+        setTestData([...testData, newRow])
+    }
+
+    function handleDelete(id: number){
+        setTestData(testData.filter((item, index) => index != id))
+    }
+
+
 
     return(
         <div className='personalBestMainDiv'>
             <div className='personalBestTitle'>
-                <PersonalButtons />
+                {/* <PersonalButtons /> */}
+                <button onClick={handleAdd}>Add</button>
             </div>
-            <div className='personalBestRecords'>
-                {PersonalMock.map((exercise, id) => {
+            <div key={'PRBODY'} className='personalBestRecords'>
+                {testData.map((exercise, id) => {
                     return(
-                        <PersonalRow id={id} date={exercise.date} weight={exercise.weight} exercise={exercise.exercise} />
+                        <PersonalRow key={id} id={id} date={exercise.date} weight={exercise.weight} exercise={exercise.exercise} deleteHandler={handleDelete} />
                     )
                 })}
             </div>
--- workout-app/src/Components/Personal Best/PersonalRow.tsx
+++ workout-app/src/Components/Personal Best/PersonalRow.tsx
@@ -1,19 +1,19 @@
 // Hook import
-import { useState, useReducer } from 'react'
+import { useState, useReducer, MouseEventHandler } from 'react'
 
 
-export function PersonalRow(rowProps : {id: number, date: string, exercise: string, weight: string}){
+export function PersonalRow(rowProps : {id: number, date: string, exercise: string, weight: string, deleteHandler: (id: number)=> void }){
     const [editShow, setEditShow] = useState<boolean>(false)
     
     return (
-        <div data-RowKEY={rowProps.id} className="personalBestEntries">
-            <div key={rowProps.id} className='personalDate' contentEditable={editShow}>{rowProps.date}</div>
-            <div key={rowProps.id} className='personalExercise' contentEditable={editShow}>{rowProps.exercise}</div>
-            <div key={rowProps.id} className='personalWeight' contentEditable={editShow}>{rowProps.weight}</div>
+        <div key={rowProps.id + 0.1} className="personalBestEntries">
+            <div key={rowProps.id + 0.2} className='personalDate' contentEditable={editShow}>{rowProps.date}</div>
+            <div key={rowProps.id + 0.3} className='personalExercise' contentEditable={editShow}>{rowProps.exercise}</div>
+            <div key={rowProps.id + 0.4} className='personalWeight' contentEditable={editShow}>{rowProps.weight}</div>
             <div className='personalBTNS'>
-                <button key={rowProps.id} className='editBTN' onClick={() => setEditShow(!editShow)}>Edit</button>
-                <button>Delete</button>
+                <button key={rowProps.id + 0.5} className='editBTN' onClick={() => setEditShow(!editShow)}>Edit</button>
+                <button key={rowProps.id + 0.6} onClick={() => {rowProps.deleteHandler(rowProps.id)}}>Delete</button>
             </div>
         </div>
     )
-}
+} 
